b7c4191ed706d1ca4c41ef8005bf53f4680ea077
Nils Gillmann <gillmann@infotropique.org>
2018-06-17 18:26:44 +0000

    setup.py
    Signed-off-by: Nils Gillmann <gillmann@infotropique.org>

--- setup.py
+++ setup.py
@@ -1,39 +1,42 @@
 #!/bin/env/python
 try:
-    from setuptools import setup
+    from setuptools import setup, find_packages
 except ImportError:
-    from distutils.core import setup
+    from distutils.core import setup, find_packages
 
 setup(
     name="gnunet",
     version="0.0",
 
-    packages=['gnunet'],
+    packages = find_packages(),
     # install_requires=['dbus-python'],
     # We require PyGObject (pygobject) but would have to
     # pass a URL to pip3 for that, as it is not on pypi.
 
-    # Maybe this should just be GNUnet eV.
-    author="ng0",
-    author_email="ng0@gnunet.org",
+    author="Nils Gillmann",
+    author_email="gillmann@gnunet.org",
     description="Python bindings for GNUnet",
     license="GNU GPLv3+",
     keywords="GNUnet binding p2p",
-    url="https://gnunet.org",
-    long_description="""GNUnet is an alternative network stack for building secure, decentralized and privacy-preserving distributed applications. Our goal is to replace the old insecure Internet protocol stack. Starting from an application for secure publication of files, it has grown to include all kinds of basic protocol components and applications towards the creation of a GNU internet.
+    url="https://gnunet.org/git/gnunet-python.git",
+    long_description="""
+GNUnet is an alternative network stack for building secure,
+decentralized and privacy-preserving distributed applications. Our
+goal is to replace the old insecure Internet protocol stack. Starting
+from an application for secure publication of files, it has grown to
+include all kinds of basic protocol components and applications
+towards the creation of a GNU internet.
 
 GNUnet is an official GNU package.
 
-This Python module provides Python bindings to GNUnet.""",
+This Python module provides Python bindings to GNUnet.
+""",
 
     classifiers=['Development Status :: 3 - Alpha',
                  'Intended Audience :: Developers',
 		 'License :: OSI Approved :: GNU General Public License (GPL)',
 		 'Operating System :: OS Independent',
-		 'Operating System :: MacOS :: MacOS X',
-		 'Operating System :: Microsoft :: Windows',
 		 'Operating System :: POSIX'],
 
-    platforms=['windows', 'Linux', 'MacOS X', 'Solaris', 'FreeBSD'],
-
+    platforms=['Linux', 'FreeBSD'],
 )
